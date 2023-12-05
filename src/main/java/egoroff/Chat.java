4ffa03257f59c35eaf6ef761f4780179030efa50
Kashira2 <nekit.egorov@icloud.com>
2023-12-05 15:03:55 +0300

    Loger add

--- src/main/java/egoroff/Chat.java
+++ src/main/java/egoroff/Chat.java
@@ -2,11 +2,13 @@ package egoroff;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Chat extends JFrame {
-    private static final int WINDOW_HEIGHT = 555;
+    private static final int WINDOW_HEIGHT = 800;
     private static final int WINDOW_WIDTH = 507;
     private static final int WINDOW_POSX = 800;
     private static final int WINDOW_POSY = 300;
@@ -15,7 +17,6 @@ public class Chat extends JFrame {
     JPanel panelSms = new JPanel(new GridLayout(5,1));
 
     JButton btnSend = new JButton("Send sms");
-    JButton btnExit = new JButton("Exit");
 
     JLabel lblLogin = new JLabel("Login");
     JLabel lblPassword = new JLabel("Password");
@@ -33,13 +34,23 @@ public class Chat extends JFrame {
     String ip;
     String massage;
 
+    char [] log = new char[500];
 
-    Chat(){
+    Chat()throws IOException{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(WINDOW_POSX, WINDOW_POSY);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Chat");
         setResizable(false);
+
+
+        FileReader reader = new FileReader("loger.txt");
+        FileWriter writer = new FileWriter("loger.txt", true);
+        reader.read(log);
+        for (char c: log) {
+            areaText.append(String.valueOf(c));
+        }
+        reader.close();
 
         btnSend.addActionListener(new ActionListener() {
             @Override
@@ -48,6 +59,32 @@ public class Chat extends JFrame {
                 massage = login + ": " + txtFieldMassage.getText() + "\n";
                 areaText.append(massage);
                 System.out.println("Отправлено сообщение: " + massage);
+
+                try {
+                    writer.write(massage);
+                    writer.flush();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        txtFieldMassage.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    login = txtFieldLogin.getText();
+                    massage = login + ": " + txtFieldMassage.getText() + "\n";
+                    areaText.append(massage);
+                    System.out.println("Отправлено сообщение: " + massage);
+
+                    try {
+                        writer.write(massage);
+                        writer.flush();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
             }
         });
 
@@ -68,5 +105,6 @@ public class Chat extends JFrame {
 
         setLayout(new GridLayout(2,1));
         setVisible(true);
+
     }
 }
--- src/main/java/egoroff/Main.java
+++ src/main/java/egoroff/Main.java
@@ -1,10 +1,12 @@
 package egoroff;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        new GameWindow();
-        System.out.println("Method main() is over");
+    public static void main(String[] args)throws IOException {
+//        new GameWindow();
+//        System.out.println("Method main() is over");
 //        new Loger();
-//        new Chat();
+        new Chat();
     }
 }
